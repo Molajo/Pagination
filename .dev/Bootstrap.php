e9d703254164af7b4b3b38d4f945fb91470d6a68
AmyStephen <AmyStephen@gmail.com>
2014-04-26 17:11:00 -0500

    packaging

--- .dev/Bootstrap.php
+++ .dev/Bootstrap.php
@@ -6,24 +6,12 @@
  * @copyright  2014 Amy Stephen. All rights reserved.
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
  */
+$base     = substr(__DIR__, 0, strlen(__DIR__) - 5);
+include $base . '/vendor/autoload.php';
 include_once __DIR__ . '/CreateClassMap.php';
 
-if (! defined('PHP_VERSION_ID')) {
-    $version = explode('.', phpversion());
-    define('PHP_VERSION_ID', ($version[0] * 10000 + $version[1] * 100 + $version[2]));
-}
-
-$base     = substr(__DIR__, 0, strlen(__DIR__) - 5);
 $classmap = array();
-$classmap = createClassMap($base . '/vendor/commonapi/render', 'CommonApi\\Render\\');
-
-$results  = createClassMap($base . '/vendor/commonapi/exception', 'CommonApi\\Exception\\');
-$classmap = array_merge($classmap, $results);
-
 $results  = createClassMap($base . '/Source', 'Molajo\\');
-$classmap = array_merge($classmap, $results);
-
-ksort($classmap);
 
 spl_autoload_register(
     function ($class) use ($classmap) {
